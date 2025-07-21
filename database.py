44cdc3ce3d515a2b5e1e310b10699be2d812a96f
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-21 20:37:17 -0300

    Update database.py

--- database.py
+++ database.py
@@ -5,10 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from typing import Generator
 
-
 conn_str = os.getenv("SQLSERVER_CONN_STR")  
 quoted = urllib.parse.quote_plus(conn_str)
 SQLALCHEMY_DATABASE_URL = f"mssql+pyodbc:///?odbc_connect={quoted}"
+
+Base = declarative_base()
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 # Creamos el engine
 engine = create_engine(
