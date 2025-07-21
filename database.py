211d567f52aa7ee517a5cc07f0d599d3d53bc747
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-21 20:28:26 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,3 +3,7 @@ load_dotenv()
 import os, urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+conn_str = os.getenv("SQLSERVER_CONN_STR")  
+quoted = urllib.parse.quote_plus(conn_str)
+SQLALCHEMY_DATABASE_URL = f"mssql+pyodbc:///?odbc_connect={quoted}"
