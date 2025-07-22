d207c97968bfb051bbef2d340578ac4be26d6124
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-21 21:29:38 -0300

    Update Sucursales_model.py

---
+++ APP/DB/Sucursales_model.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint
+from database import Base 
+from sqlalchemy.orm import relationship
+from datetime import datetime
