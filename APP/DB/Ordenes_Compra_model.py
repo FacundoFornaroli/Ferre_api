57e12ce6ad7eb4b951dba45a1df5995d2ca51b0a
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-21 23:23:45 -0300

    Update Ordenes_Compra_model.py

--- APP/DB/Ordenes_Compra_model.py
+++ APP/DB/Ordenes_Compra_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint, Numeric, Text
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint, Numeric, Text, Date
 from database import Base 
 from sqlalchemy.orm import relationship
 from datetime import datetime
