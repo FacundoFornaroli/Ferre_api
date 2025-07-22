ea921c0249925d72a30a9f67b50b76a4e1919fc5
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-21 21:27:56 -0300

    Update Unidades_de_medida_model.py

--- APP/DB/Unidades_de_medida_model.py
+++ APP/DB/Unidades_de_medida_model.py
@@ -2,3 +2,13 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, C
 from database import Base 
 from sqlalchemy.orm import relationship
 from datetime import datetime
+
+
+class Unidades_de_medida(Base):
+    __tablename__ = "Unidades_de_medida"
+    ID_Unidad_de_medida = Column(Integer, primary_key=True, index=True)
+    Nombre = Column(String(50), nullable=False, unique=True)
+    Abreviatura = Column(String(10), nullable=False, unique=True)
+    Activo = Column(Boolean, default=True, nullable=False)
+
+CheckConstraint(Nombre == Abreviatura, name='check_nombre_abreviatura')
