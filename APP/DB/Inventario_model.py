70f2749e83d8bc3fa7926b18618e4b13d29b0bd3
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-21 22:09:00 -0300

    Update Inventario_model.py

---
+++ APP/DB/Inventario_model.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint, Numeric, Text
+from database import Base 
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+class
