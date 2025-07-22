cdf4f96ed03e1ca3a97f7cf83662f6517a35a269
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-21 21:44:07 -0300

    Update Usuarios_model.py

--- APP/DB/Usuarios_model.py
+++ APP/DB/Usuarios_model.py
@@ -1,4 +1,9 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint, true
 from database import Base 
 from sqlalchemy.orm import relationship
 from datetime import datetime
+
+class Usuarios(Base):
+    __tablename__ = "Usuarios"
+    ID_Usuario = Column(Integer, primary_key=True, index=True)
+    
