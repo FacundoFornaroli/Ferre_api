eb36abbe07e0e0c12d29c705bbc438e9e0c61bc9
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-22 23:07:21 -0300

    Update Detalles_Devoluciones_model.py

---
+++ APP/DB/Detalles_Devoluciones_model.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint, Numeric, Text, Index
+from sqlalchemy.orm import relationship
+from database import Base 
+from datetime import datetime
