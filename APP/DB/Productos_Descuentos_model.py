78f3353848cd5938e72ae0a0377d84b1c3d3c104
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-22 21:35:18 -0300

    Update Productos_Descuentos_model.py

---
+++ APP/DB/Productos_Descuentos_model.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, CheckConstraint, Numeric, Text, Index
+from database import Base 
+from sqlalchemy.orm import relationship
+from datetime import datetime
