3ab1edba30efee0036d8fd99a8eacc164b2220f4
Facundo Fornaroli Fuhr <fornarolifuhrfacundo@gmail.com>
2025-07-27 21:27:28 -0300

    Update Categorias_router.py

--- APP/routers/Categorias_router.py
+++ APP/routers/Categorias_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Query, Path
 from sqlalchemy.orm import Session
 from typing import List, Optional
-from ...database import get_db  # Un nivel más arriba con tres puntos
+from database import get_db
 from ..schemas.Categorias_schema import CategoriaBase, CategoriaCreate, CategoriaUpdate, CategoriaSimple, CategoriaCompleta, CategoriaList, CategoriaEstadisticas
 from ..DB.Categorias_model import Categorias  # Note que también es 'Categorias', no 'Categoria'
 from sqlalchemy import func
